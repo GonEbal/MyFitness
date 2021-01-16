2eb31fda27c03703b9260364b6d1ed0959ffbeb3
GonEbal <66447247+GonEbal@users.noreply.github.com>
2021-01-15 17:44:12 -0800

    Update AddEntry and helpers.

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import * as React from "react"
+import { View } from "react-native"
+import AddEntry from './components/AddEntry'
 
 export default class App extends React.Component {
-  componentDidMount() {
-    console.log("Before");
-    debugger
-    console.log("After");
-  }
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>Deniska</Text>
+      <View>
+        <AddEntry />
       </View>
-    );
+    )
   }
 }
