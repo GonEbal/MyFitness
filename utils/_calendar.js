f890054adbf3c3a210af2f3f7dfe3f89005b1748
GonEbal <66447247+GonEbal@users.noreply.github.com>
2021-01-17 16:29:54 -0800

    Changes for Api

--- utils/_calendar.js
+++ utils/_calendar.js
@@ -46,7 +46,7 @@ Finally, paste the following code inside of your utils/_calendar.js file.
 import { AsyncStorage } from 'react-native'
 import { getMetricMetaInfo, timeToString } from './helpers'
 
-export const CALENDAR_STORAGE_KEY = 'UdaciFitness:calendar'
+export const CALENDAR_STORAGE_KEY = 'Fitness:calendar'
 
 function getRandomNumber (max) {
   return Math.floor(Math.random() * max) + 0
