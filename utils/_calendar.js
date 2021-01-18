577c2d4940f13e4e79e0d9f9bebbfe77e5531d21
GonEbal <66447247+GonEbal@users.noreply.github.com>
2021-01-17 16:53:27 -0800

    Typo error

--- utils/_calendar.js
+++ utils/_calendar.js
@@ -1,52 +1,7 @@
-export function isBetween (num, x, y) {
-  if (num >= x && num <= y) {
-    return true
-  }
-
-  return false
-}
-
-export function calculateDirection (heading) {
-  let direction = ''
-
-  if (isBetween(heading, 0, 22.5)) {
-    direction = 'North'
-  } else if (isBetween(heading, 22.5, 67.5)) {
-    direction = 'North East'
-  } else if (isBetween(heading, 67.5, 112.5)) {
-    direction = 'East'
-  } else if (isBetween(heading, 112.5, 157.5)) {
-    direction = 'South East'
-  } else if (isBetween(heading, 157.5, 202.5)) {
-    direction = 'South'
-  } else if (isBetween(heading, 202.5, 247.5)) {
-    direction = 'South West'
-  } else if (isBetween(heading, 247.5, 292.5)) {
-    direction = 'West'
-  } else if (isBetween(heading, 292.5, 337.5)) {
-    direction = 'North West'
-  } else if (isBetween(heading, 337.5, 360)) {
-    direction = 'North'
-  } else {
-    direction = 'Calculating'
-  }
-
-  return direction
-}
-
-export function timeToString (time = Date.now()) {
-  const date = new Date(time)
-  const todayUTC = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
-  return todayUTC.toISOString().split('T')[0]
-}
-Finally, paste the following code inside of your utils/_calendar.js file.
-
-// utils/_calendar.js
-
 import { AsyncStorage } from 'react-native'
 import { getMetricMetaInfo, timeToString } from './helpers'
 
-export const CALENDAR_STORAGE_KEY = 'Fitness:calendar'
+export const CALENDAR_STORAGE_KEY = 'MyFitness:calendar'
 
 function getRandomNumber (max) {
   return Math.floor(Math.random() * max) + 0
