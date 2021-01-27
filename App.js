4bb214bdd7e3c5f7fb045f9324487dcf3bb1d199
GonEbal <66447247+GonEbal@users.noreply.github.com>
2021-01-27 12:46:28 -0800

    Recover

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createStore } from "redux"
 import { Provider } from "react-redux"
 import reducer from "./reducers"
 import History from './components/History'
+import { createBottomTabNavigator } from 'react-navigation'
 
 export default class App extends React.Component {
 	render() {
