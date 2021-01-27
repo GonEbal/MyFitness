062abd501dd8fe079b00c62e37e15cebfbfb7032
GonEbal <66447247+GonEbal@users.noreply.github.com>
2021-01-27 11:55:45 -0800

    To delete

--- App.js
+++ App.js
@@ -4,16 +4,13 @@ import AddEntry from "./components/AddEntry"
 import { createStore } from "redux"
 import { Provider } from "react-redux"
 import reducer from "./reducers"
-import History from './components/History'
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<Provider store={createStore(reducer)}>
-				<View style={{flex: 1}}>
-					<View style={{height: 50}}/>
-					<History />
-					
+				<View>
+					<AddEntry />
 				</View>
 			</Provider>
 		)
--- components/Slider.js
+++ components/Slider.js
@@ -1,35 +1,20 @@
 import React from "react"
-import { View, Text, Slider, StyleSheet } from "react-native"
-import { gray } from '../utils/colors'
+import { View, Text, Slider } from "react-native"
 
 export default function Slidder({ max, unit, step, value, onChange }) {
 	return (
-		<View style={styles.row}>
+		<View>
 			<Slider
-				style={{flex: 1}}
 				step={step}
 				value={value}
 				maximumValue={max}
 				minimumValue={0}
 				onValueChange={onChange}
 			/>
-			<View style={styles.metricCounter}>
-				<Text style={{fontSize: 24, textAlign: 'center'}}>{value}</Text>
-				<Text style={{fontSize: 18, textAlign: gray}}>{unit}</Text>
+			<View>
+				<Text>{value}</Text>
+				<Text>{unit}</Text>
 			</View>
 		</View>
 	)
 }
-
-const styles = StyleSheet.create({
-	row: {
-		flexDirection: "row",
-		flex: 1,
-		alignItems: "center",
-	},
-	metricCounter: {
-		width: 85,
-		justifyContent: 'center',
-		alignItems: 'center',
-	}
-})
