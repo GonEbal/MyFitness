9202368269fab515a6784d16c7366f8f4ffe2aef
GonEbal <66447247+GonEbal@users.noreply.github.com>
2021-01-27 15:14:43 -0800

    Navigation Bar

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
 import React from "react"
-import { View } from "react-native"
+import { View, Platform } from "react-native"
 import AddEntry from "./components/AddEntry"
 import { createStore } from "redux"
 import { Provider } from "react-redux"
 import reducer from "./reducers"
-import History from './components/History'
-import { createBottomTabNavigator } from 'react-navigation'
+import History from "./components/History"
+import { NavigationContainer } from "@react-navigation/native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { FontAwesome, Ionicons } from "@expo/vector-icons"
+
+const Tab = createBottomTabNavigator()
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<Provider store={createStore(reducer)}>
-				<View style={{flex: 1}}>
-					<View style={{height: 50}}/>
-					<History />
-					
+				<View style={{ flex: 1 }}>
+					<View style={{ height: 50 }} />
+					<NavigationContainer>
+						<Tab.Navigator
+							screenOptions={({ route }) => ({
+								tabBarIcon: ({ focused, color, size }) => {
+									let iconName
+
+									if (route.name === "History") {
+										iconName = focused
+											? "ios-bookmarks"
+											: "ios-bookmarks-outline"
+									} else if (route.name === "AddEntry") {
+										iconName = focused
+											? "add-circle"
+											: "add-circle-outline"
+									}
+									return (
+										<Ionicons
+											name={iconName}
+											size={30}
+											color={color}
+										/>
+									)
+								},
+							})}
+							tabBarOptions={{
+								activeTintColor: "#00BBF2",
+								inactiveTintColor: "gray",
+							}}
+						>
+							<Tab.Screen name="History" component={History} />
+							<Tab.Screen name="AddEntry" component={AddEntry} />
+						</Tab.Navigator>
+					</NavigationContainer>
 				</View>
 			</Provider>
 		)
